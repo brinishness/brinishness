d1928b2e88546ba0d6349dfddbb8dd85f119ff76
tears <596231290@qq.com>
2022-08-09 01:46:09 +0800

    添加新建文件

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const moment = require("moment");
+var fs = require("fs");
 console.log("hello world");
 console.log(moment().add(1).toObject())
+fs.writeFile("test.txt","this is a test", function (err){
+    console.log(err);
+})
